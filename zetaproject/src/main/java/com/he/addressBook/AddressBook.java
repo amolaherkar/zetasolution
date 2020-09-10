3aeab2fdbc65515a161ed6f6971b987d06ff33e7
amolaherkar <40294149+amolaherkar@users.noreply.github.com>
2020-09-10 10:26:46 +0530

    Add files via upload

---
+++ zetaproject/src/main/java/com/he/addressBook/AddressBook.java
@@ -0,0 +1,39 @@
+package com.he.addressBook;
+
+import java.util.List;
+
+public class AddressBook {
+
+    public AddressBook() {
+        //TODO
+    	//write business logic as per the question 
+    }
+
+    public void addContact(Contact contact) {
+        //TODO
+    	//write business logic as per the question 
+    }
+
+    public void deleteContact(String name) {
+        //TODO
+    	//write business logic as per the question 
+    }
+
+    public void updateContact(String name, Contact contact) {
+        //TODO
+    	//write business logic as per the question 
+    }
+
+    public List<Contact> searchByName(String name) {
+        //TODO
+    	//write business logic as per the question 
+        return null;
+    }
+
+    public List<Contact> searchByOrganisation(String organisation) {
+        //TODO
+    	//write business logic as per the question 
+        return null;
+    }
+
+}
